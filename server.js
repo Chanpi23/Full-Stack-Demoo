852587250c03503694645227f74f0c798bcbed7b
chanpi23 <chandrikad34@gmail.com>
2022-06-22 21:00:33 -0400

    addmiddleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require ('cors')
 const MongoClient = require ('mongodb').MongoClient
 require('dotenv').config()
+const PORT=8000
 
 let db,
 dbConnectionString =  process.env.DB_STRING,
@@ -16,9 +17,16 @@ MongoClient.connect(dbConnectionString)
     collection = db.collection('movies')
 })
 
-//DB_STRING
+
+app.set ('view engine', ' ejs' )// this sets up for HTML template
+app.uses(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())// parsing our data with json
+app.use(cors())// don't need to declare the library anymore
+
+
 //PORT=8000
 
 app.listen(process.env.PORT || PORT,  () => {
-    console.log (`Sever is running on port`)
+    console.log (`Sever is running on port`) 
 })
